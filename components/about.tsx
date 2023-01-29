00fb72c8ba8b8e4e23e0fdfc9929578320e075d0
Chi-Wei Lien <wilsonwilson3124@gmail.com>
2023-01-29 15:00:36 -0500

    add different section

---
+++ components/about.tsx
@@ -0,0 +1,28 @@
+const About = () => {
+    return (
+        <div className="flex justify-center h-screen place-items-center bg-neutral-100 max-w-screen">
+            <div className="w-4/5 border-8 border-black h-4/5">
+                <div className="flex justify-end w-full h-8 border-b-8 border-black">
+                    <div className="flex justify-center w-8 border-l-8 border-black place-items-center">
+                        <div className="w-4 h-1 bg-black"></div>
+                    </div>
+                    <div className="flex justify-center w-8 border-l-8 border-black place-items-center">
+                        <div className="w-4 h-4 border-4 border-black"></div>
+                    </div>
+                    <div className="flex justify-center w-8 text-xl font-bold border-l-8 border-black place-items-center">
+                        <div className='cross'>X</div>
+                    </div>
+                </div>
+                <div className="flex flex-col items-center justify-center h-full p-4 font-mono font-medium text-9xl sm:text-6xl md:text-9xl">
+                    <div className="w-fit
+                        bg-gradient-to-r from-amber-400 to-amber-400
+                        bg-no-repeat [background-position:0_100%]
+                        [background-size:100%_0.2em]"
+                    >About</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,8 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import Header from 'components/header'
 import HeaderV2 from 'components/header-v2'
+import About from 'components/about'
+import Sponsors from 'components/sponsors'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,6 +12,8 @@ export default function Home() {
   return (
     <>
       <HeaderV2 />
+      <About></About>
+      <Sponsors></Sponsors>
     </>
   )
 }
