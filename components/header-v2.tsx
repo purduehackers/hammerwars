fa57c906b0baa6121cc1c3af9036c3c838d19d89
Chi-Wei Lien <wilsonwilson3124@gmail.com>
2023-01-29 13:39:33 -0500

    add window bars

--- components/header-v2.tsx
+++ components/header-v2.tsx
@@ -3,9 +3,15 @@ const HeaderV2 = () => {
         <div className="flex justify-center h-screen place-items-center bg-neutral-100 max-w-screen">
             <div className="w-4/5 border-8 border-black h-4/5">
                 <div className="flex justify-end w-full h-8 border-b-8 border-black">
-                    <div className="w-8 border-l-8 border-black"></div>
-                    <div className="w-8 border-l-8 border-black"></div>
-                    <div className="w-8 border-l-8 border-black"></div>
+                    <div className="w-8 border-l-8 border-black flex justify-center place-items-center">
+                        <div className="w-4 h-1 bg-black"></div>
+                    </div>
+                    <div className="w-8 border-l-8 border-black flex justify-center place-items-center">
+                        <div className="w-4 h-4 border-4 border-black"></div>
+                    </div>
+                    <div className="w-8 border-l-8 border-black flex justify-center place-items-center font-bold text-xl">
+                        <div className='cross'>x</div>
+                    </div>
                 </div>
                 <div className="flex flex-col justify-center h-full p-4 font-mono font-medium">
                     <div className="text-4xl w-fit sm:text-6xl md:text-title
