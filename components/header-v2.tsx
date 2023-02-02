af54fdf92c5fd59dc595387af87cd77adea507ba
Chi-Wei Lien <wilsonwilson3124@gmail.com>
2023-02-01 19:58:10 -0500

    add gridlines

--- components/header-v2.tsx
+++ components/header-v2.tsx
@@ -1,19 +1,20 @@
+
 const HeaderV2 = () => {
     return (
-        <div className="flex justify-center h-screen place-items-center bg-neutral-100 max-w-screen">
-            <div className="w-4/5 border-8 border-black h-4/5">
-                <div className="flex justify-end w-full h-8 border-b-8 border-black">
-                    <div className="w-8 border-l-8 border-black flex justify-center place-items-center">
+        <div className="flex justify-center h-screen place-items-center max-w-screen">
+            <div className="w-4/5 bg-white border-8 border-black h-4/5">
+                <div className="flex justify-end w-full h-8 border-b-8 border-black bg-amber-200">
+                    <div className="flex justify-center w-8 border-l-8 border-black place-items-center">
                         <div className="w-4 h-1 bg-black"></div>
                     </div>
-                    <div className="w-8 border-l-8 border-black flex justify-center place-items-center">
+                    <div className="flex justify-center w-8 border-l-8 border-black place-items-center">
                         <div className="w-4 h-4 border-4 border-black"></div>
                     </div>
-                    <div className="w-8 border-l-8 border-black flex justify-center place-items-center font-bold text-xl">
+                    <div className="flex justify-center w-8 text-xl font-bold border-l-8 border-black place-items-center">
                         <div className='cross'>X</div>
                     </div>
                 </div>
-                <div className="flex flex-col justify-center items-center h-full p-4 font-mono font-medium text-9xl sm:text-6xl md:text-9xl">
+                <div className="flex flex-col items-center justify-center h-full p-4 font-mono text-6xl font-medium sm:text-7xl md:text-9xl">
                     <div className="w-fit
                         bg-gradient-to-r from-green-400 to-green-400
                         bg-no-repeat [background-position:0_100%]
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,8 +12,8 @@ export default function Home() {
   return (
     <>
       <HeaderV2 />
-      <About></About>
-      <Sponsors></Sponsors>
+      {/* <About></About> */}
+      {/* <Sponsors></Sponsors> */}
     </>
   )
 }
