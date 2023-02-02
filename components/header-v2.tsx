567383778209719fb4059c2f4da1e4ab3fbe4186
Chi-Wei Lien <wilsonwilson3124@gmail.com>
2023-02-01 20:10:32 -0500

    add draggable

--- components/header-v2.tsx
+++ components/header-v2.tsx
@@ -1,32 +1,39 @@
+import { useContext } from "react"
+import Draggable from "react-draggable"
+
+import { DraggableContext } from '../context/draggable-context'
 
 const HeaderV2 = () => {
+    const { draggable } = useContext(DraggableContext);
     return (
         <div className="flex justify-center h-screen place-items-center max-w-screen">
-            <div className="w-4/5 bg-white border-8 border-black h-4/5">
-                <div className="flex justify-end w-full h-8 border-b-8 border-black bg-amber-200">
-                    <div className="flex justify-center w-8 border-l-8 border-black place-items-center">
-                        <div className="w-4 h-1 bg-black"></div>
+            <Draggable disabled={!draggable} handle=".handle">
+                <div className="w-4/5 bg-white border-8 border-black h-4/5">
+                    <div className="flex justify-end w-full h-8 border-b-8 border-black handle bg-amber-200">
+                        <div className="flex justify-center w-8 border-l-8 border-black place-items-center">
+                            <div className="w-4 h-1 bg-black"></div>
+                        </div>
+                        <div className="flex justify-center w-8 border-l-8 border-black place-items-center">
+                            <div className="w-4 h-4 border-4 border-black"></div>
+                        </div>
+                        <div className="flex justify-center w-8 text-xl font-bold border-l-8 border-black place-items-center">
+                            <div className='cross'>X</div>
+                        </div>
                     </div>
-                    <div className="flex justify-center w-8 border-l-8 border-black place-items-center">
-                        <div className="w-4 h-4 border-4 border-black"></div>
-                    </div>
-                    <div className="flex justify-center w-8 text-xl font-bold border-l-8 border-black place-items-center">
-                        <div className='cross'>X</div>
+                    <div className="flex flex-col items-center justify-center h-full p-4 font-mono text-6xl font-medium sm:text-7xl md:text-9xl">
+                        <div className="w-fit
+                            bg-gradient-to-r from-green-400 to-green-400
+                            bg-no-repeat [background-position:0_100%]
+                            [background-size:100%_0.2em]"
+                        >Hammer</div>
+                        <div className="w-fit
+                            bg-gradient-to-r from-green-400 to-green-400
+                            bg-no-repeat [background-position:0_100%]
+                            [background-size:100%_0.2em]"
+                        >Wars_</div>
                     </div>
                 </div>
-                <div className="flex flex-col items-center justify-center h-full p-4 font-mono text-6xl font-medium sm:text-7xl md:text-9xl">
-                    <div className="w-fit
-                        bg-gradient-to-r from-green-400 to-green-400
-                        bg-no-repeat [background-position:0_100%]
-                        [background-size:100%_0.2em]"
-                    >Hammer</div>
-                    <div className="w-fit
-                        bg-gradient-to-r from-green-400 to-green-400
-                        bg-no-repeat [background-position:0_100%]
-                        [background-size:100%_0.2em]"
-                    >Wars_</div>
-                </div>
-            </div>
+            </Draggable>
         </div>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,59 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import Header from 'components/header'
+import { useState, useEffect, useMemo } from 'react'
+
 import HeaderV2 from 'components/header-v2'
 import About from 'components/about'
 import Sponsors from 'components/sponsors'
-
-const inter = Inter({ subsets: ['latin'] })
+import { DraggableContext, DraggableInterface } from 'context/draggable-context'
 
 export default function Home() {
+
+  // Disable draggable feature on small screen
+  const [windowSize, setWindowSize] = useState({
+    width: 0,
+    height: 0
+  })
+  const [draggable, setDraggable] = useState<boolean>(false)
+
+  const value = useMemo<DraggableInterface>(
+    () => ({ draggable, setDraggable }),
+    [draggable]
+  )
+
+  const resizeHandler = () => {
+    setWindowSize({
+      width: window.innerWidth,
+      height: window.innerHeight
+    })
+
+    if (windowSize.width > 640) {
+      setDraggable(true)
+    } else {
+      setDraggable(false)
+    }
+  }
+
+  useEffect((): any => {
+    if (window.innerWidth > 640) {
+      setDraggable(true)
+    } else {
+      setDraggable(false)
+    }
+  }, [])
+
+  useEffect((): any => {
+    window.addEventListener('resize', resizeHandler)
+    return () => {
+      window.removeEventListener('resize', resizeHandler)
+    }
+  })
+
   return (
-    <>
+    <DraggableContext.Provider value={value}>
       <HeaderV2 />
       {/* <About></About> */}
       {/* <Sponsors></Sponsors> */}
-    </>
+    </DraggableContext.Provider>
   )
 }
