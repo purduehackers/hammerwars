405e13e5698efeef504bf0e7cae198c8224cdcd4
Matthew Stanciu <mattbstanciu@gmail.com>
2023-03-09 19:53:43 -0500

    Copy some of the data from site

--- utils/data.ts
+++ utils/data.ts
@@ -1,15 +1,34 @@
 export const madeWith = [
-    'love',
-    'an army of fearless geese',
-    'coffee and sleepless nights',
-    'a mix of caffeinated and herbal tea',
-    'heart and zap'
-]
+  "love",
+  "an army of fearless geese",
+  "coffee and sleepless nights",
+  "a mix of caffeinated and herbal tea",
+  "heart and zap",
+  "dihydrogen monoxide",
+  "a half-eaten sandwich",
+  "a top secret mixture lost to mankind",
+  "a spatula",
+  "maximum strength anti-dandruff shampoo",
+  "AWESOMENESS",
+  "a forbidden spell",
+];
 
 export const faqData = [
-    ['Do I need any experience?', 'No prior experience.'],
-    ['Is this competition limited to only Computer Science majors?', 'This competition is open to all majors.'],
-    ['Is this a Hackathon?', 'Nope, you will be solving coding interview problems through the CodeForces website. Maybe in another competition.'],
-    ['Are we able to work in groups?', 'You will tackle programming problems individually.'],
-    ['I\'m not quite sure what level I\'m fit for, how can I change that?', 'Whatever level you\'ve indicated on the registration form does not matter! This is for us to estimate how many people we\'ll expect per level. You will get to choose your level on the day-of the competition!']
-]
+  ["Do I need any experience?", "No prior experience."],
+  [
+    "Is this competition limited to only Computer Science majors?",
+    "This competition is open to all majors.",
+  ],
+  [
+    "Is this a Hackathon?",
+    "Nope, you will be solving coding interview problems through the CodeForces website. Maybe in another competition.",
+  ],
+  [
+    "Are we able to work in groups?",
+    "You will tackle programming problems individually.",
+  ],
+  [
+    "I'm not quite sure what level I'm fit for, how can I change that?",
+    "Whatever level you've indicated on the registration form does not matter! This is for us to estimate how many people we'll expect per level. You will get to choose your level on the day-of the competition!",
+  ],
+];
